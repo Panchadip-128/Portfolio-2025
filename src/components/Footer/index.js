73caac013237c2abe02c2fa915ea35bd4d225d85
Panchadip-128 <panchadip128@gmail.com>
2025-04-18 23:29:59 +0530

    Updations on location

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -110,7 +110,7 @@ function Footer() {
             title="Google Maps Location"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3885.6187441141324!2d77.58961367518202!3d13.12332208720612!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae19d502aa02b3%3A0xb31f075ede4c71ab!2sManipal%20Academy%20of%20Higher%20Education%2C%20Bengaluru!5e0!3m2!1sen!2sin!4v1744996817279!5m2!1sen!2sin"
             width="100%"
-            height="300"
+            height="230"
             style={{ border: 0 }}
             allowFullScreen=""
             loading="lazy"
