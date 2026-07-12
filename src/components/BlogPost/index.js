a77c44a686b8945c4c77482075b80f98a6d7e4ba
Panchadip-128 <panchadip.mitblr2023@learner.manipal.edu>
2026-07-12 23:36:19 +0530

    fix: change cover image to height auto for perfect responsive scaling

--- src/components/BlogPost/index.js
+++ src/components/BlogPost/index.js
@@ -21,10 +21,9 @@ const Container = styled.div`
 
 const CoverImage = styled(motion.img)`
   width: 100%;
-  height: 60vh;
-  object-fit: contain;
-  background-color: #0b0b10;
-  @media (max-width: 768px) { height: 40vh; }
+  height: auto;
+  display: block;
+  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.5);
 `;
 
 const ContentWrapper = styled(motion.div)`
